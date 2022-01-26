739cd19399360cf941f531f0d3332953c285a5b9
Kai Li <2612733l@student.gla.ac.uk>
2022-01-26 21:12:00 +0000

    The completed chapter 3 exercises: add an about page

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 # import the Response from the django.http module
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
