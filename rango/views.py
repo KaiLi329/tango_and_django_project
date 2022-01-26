f57d1c49af3e5314fd537b6c8e49d265e6df6e1a
Kai Li <2612733l@student.gla.ac.uk>
2022-01-26 21:42:45 +0000

    Add a template index.html via dynamic path

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,12 @@ from django.http import HttpResponse
 # import the Response from the django.http module
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # Construct a dictionary to pass to the template engine as its context.
+    # the key boldmessage matches to {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    #             request    template file name   context dictionary
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
